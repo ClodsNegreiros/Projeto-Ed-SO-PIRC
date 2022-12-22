c656d854c6bb49d0a2480733c5136a3829b089cb
Clodoaldo Negreiros <96806687+ClodsNegreiros@users.noreply.github.com>
2022-12-21 22:41:22 -0300

    Jogo última atualização

---
+++ ProtocolCCC.py
@@ -0,0 +1,15 @@
+msg_success = {
+            '[C-WIN]': 'Jogo Vencido! ',
+            '[C-SENT]': "Letra enviada com sucesso!", 
+}
+
+msg_error = {
+            '[C-INVALID]': "Letra inválida!",
+            '[C-NONEXISTENT]': "Letra não existe na palavra!",
+            '[C-LOSE]': "Jogo Perdido!",
+}
+
+msg_game = {
+            '[C-INSERT]': "Digite uma letra: ",
+            '[C-OPTION]': "Digite S para jogar novamente: ",
+}
